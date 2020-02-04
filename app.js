5966f4362500c39ad73fb8619588f4f54288584f
Joe Lynn <josephlynn94@gmail.com>
2020-02-04 18:31:42 -0500

    added route to get notes html page

--- app.js
+++ app.js
@@ -4,17 +4,20 @@ const path = require("path");
 
 
 const app = express();
-app.use(express.static('public'))
 const PORT = process.env.PORT || 8080;
 
 app
+	.use(express.static("public"))
 	.use(express.urlencoded({ extended: true }))
 	.use(express.json());
 
-app.use('/*', (req, res) => {
-	res.sendfile(path.join(__dirname, './public/index.html'));
+app.get('/', (req, res) => {
+	res.sendfile(path.join(__dirname, "./public/index.html"));
 });
 
+app.get("/notes", (req, res) => {
+	res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
 
 app.listen(PORT, () => {
 	console.log(`App listening on PORT ${PORT}`)
