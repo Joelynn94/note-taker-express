a57d74f1e523f51e75cf38023c5f2307e69ae6ee
Joseph Lynn <josephlynn94@gmail.com>
2020-02-04 23:16:11 -0500

    changed file structure and added additional api route

--- app.js
+++ app.js
@@ -1,24 +1,40 @@
+// Dependencies 
+// ===================================================
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
 
-
+// Sets up the Express App
+// ===================================================
 const app = express();
 const PORT = process.env.PORT || 8080;
 
 app
-	.use(express.static("public"))
 	.use(express.urlencoded({ extended: true }))
+	.use(express.static("public"))
 	.use(express.json());
 
-app.get('/', (req, res) => {
+// Routes
+// ===================================================
+
+// Displays the index page
+app.get('/', (_, res) => {
 	res.sendfile(path.join(__dirname, "./public/index.html"));
 });
 
-app.get("/notes", (req, res) => {
+// Displays the notes page
+app.get("/notes", (_, res) => {
 	res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
+// Displays the api route for the notes 
+app.get("/api/notes", (_, res) => {
+	const storedData = fs.readFileSync("./public/db/db.json", "utf8");
+	return res.json(JSON.parse(storedData));
+});
+
+// Starts the server to begin listening on the port
+// ===================================================
 app.listen(PORT, () => {
 	console.log(`App listening on PORT ${PORT}`)
 });
